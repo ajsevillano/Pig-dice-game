54945b696e5f6a38f57ad3e7dbbf620692d195f0
Sevi <correosevi@gmail.com>
2020-05-11 12:13:17 +0100

    Dice working on DOM

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
-let Score, RoundScore, activePlayer, dice;
+let Score, roundScore, activePlayer;
 
 Score = [0, 0];
-RoundScore = 0;
+roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-console.log(dice);
+document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', () => {
+	// Get a random number between 1 and 6
+	let dice = Math.floor(Math.random() * 6) + 1;
+
+	// Display de result
+	let diceDOM = document.querySelector('.dice');
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'img/dice/dice-' + dice + '.png';
+
+	// Update the round score only if the random number is not 1
+	if (dice !== 1) {
+		//Add score
+		roundScore += dice;
+		document.querySelector('#current-' + activePlayer).textContent = roundScore;
+	} else {
+		//Next player
+		activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+		roundScore = 0;
+	}
+});
